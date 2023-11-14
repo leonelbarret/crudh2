43f780f756d1d959c416de5ade0509ac631f26fe
Leonel Barreto <leonelbarret@gmail.com>
2023-11-14 20:27:14 -0300

    Implementando banco de teste H2

--- src/main/java/dev/leomarques/services/UserServices.java
+++ src/main/java/dev/leomarques/services/UserServices.java
@@ -1,6 +1,8 @@
 package dev.leomarques.services;
 
 import dev.leomarques.entities.User;
+import dev.leomarques.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/users")
 public class UserServices {
+
+    @Autowired
+    UserRepository repo;
 
     @GetMapping
     public ResponseEntity<User> findAll(){
