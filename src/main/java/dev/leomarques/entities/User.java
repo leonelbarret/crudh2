0aa22f2953a271d52d7869c3138616f6124c999f
Leonel Barreto <leonelbarret@gmail.com>
2023-11-16 20:33:38 -0300

    Primeira parte, table order

--- src/main/java/dev/leomarques/entities/User.java
+++ src/main/java/dev/leomarques/entities/User.java
@@ -2,6 +2,8 @@ package dev.leomarques.entities;
 
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.*;
@@ -17,6 +19,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {}
 
@@ -66,6 +71,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
