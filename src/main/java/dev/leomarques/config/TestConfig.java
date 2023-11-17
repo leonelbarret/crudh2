1775547051ade740d0bfd1da83577cf0dd04e767
Leonel Barreto <leonelbarret@gmail.com>
2023-11-16 21:09:27 -0300

    Implements OrderStatus

--- src/main/java/dev/leomarques/config/TestConfig.java
+++ src/main/java/dev/leomarques/config/TestConfig.java
@@ -2,6 +2,7 @@ package dev.leomarques.config;
 
 import dev.leomarques.entities.Order;
 import dev.leomarques.entities.User;
+import dev.leomarques.entities.enums.OrderStatus;
 import dev.leomarques.repository.OrderRepository;
 import dev.leomarques.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.PAID, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.PAID, u1);
 
         repo.saveAll(Arrays.asList(u1,u2));
         orepo.saveAll(Arrays.asList(o1,o2,o3));
--- src/main/java/dev/leomarques/entities/Order.java
+++ src/main/java/dev/leomarques/entities/Order.java
@@ -1,5 +1,6 @@
 package dev.leomarques.entities;
 
+import dev.leomarques.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,15 +17,18 @@ public class Order implements Serializable {
     private Long id;
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
     public Order() {}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -50,6 +54,14 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus.getCode();
     }
 
     @Override
